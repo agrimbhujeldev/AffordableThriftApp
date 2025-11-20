0ed6b3fd1fde17fb5210f89cdd61a28d6ed44cdc
agrimbhujeldev <agrimbhujel2023@gmail.com>
2025-11-20 17:01:15 +0545

    Add files via upload

---
+++ flutterapp/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\saurav\dev\tooling\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\src_dev\flutter\AFFORDABLE-THRIFT-FLUTTER-APP-2025-\CODE-\SRC-\affordablethrift"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
